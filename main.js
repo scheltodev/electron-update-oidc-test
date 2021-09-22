7dc4524471f0576e8a7bae847b704804c9ae39c0
Tobias Schellenberger <schletodev@gmail.com>
2021-09-22 23:32:12 +0200

    update

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
-const { app, ipcMain } = require("electron");
+const { app, ipcMain } = require('electron')
+const { autoUpdater } = require('electron-updater')
 
-const { createAuthWindow } = require("./main/auth-process");
-const createAppWindow = require("./main/app-process");
-const authService = require("./services/auth-service");
+const { createAuthWindow } = require('./main/auth-process')
+const createAppWindow = require('./main/app-process')
+const authService = require('./services/auth-service')
 
 async function showWindow() {
   try {
-    await authService.refreshTokens();
-    return createAppWindow();
+    await authService.refreshTokens()
+    return createAppWindow()
   } catch (err) {
-    createAuthWindow();
+    createAuthWindow()
   }
 }
 
-ipcMain.on("app_version", (event) => {
-  event.sender.send("app_version", { version: app.getVersion() });
-});
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() })
+})
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall()
+})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", showWindow);
+app.on('ready', showWindow)
 
 // Quit when all windows are closed.
-app.on("window-all-closed", () => {
-  app.quit();
-});
+app.on('window-all-closed', () => {
+  app.quit()
+})
